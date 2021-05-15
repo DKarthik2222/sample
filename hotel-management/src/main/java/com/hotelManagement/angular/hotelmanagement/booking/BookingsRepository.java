000e27d31385ffc1d7d28deada0f4b4a26d5b46b
Karthik Dulam <63772598+DKarthik2222@users.noreply.github.com>
2021-05-15 22:51:34 +0530

    Delete hotel-management directory

--- hotel-management/src/main/java/com/hotelManagement/angular/hotelmanagement/booking/BookingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelManagement.angular.hotelmanagement.booking;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingsRepository extends MongoRepository<Bookings, String> {}
